097e889948ca879ade3668adcae69109925b0058
Justinas Runevicius <justinas.runevicius@ktu.edu>
2023-03-20 17:24:28 +0200

    Connected map api that dislays map and shows coordinates

---
+++ mod/gpshunt/JavaScript/userlocation.js
@@ -0,0 +1,49 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * @module    mod_gpshunt/JavaScript
+ * @package   mod_qrhunt
+ * @copyright Justinas Runevičius <justinas.runevicius@distance.ktu.lt>
+ * @author Justinas Runevičius <justinas.runevicius@distance.ktu.lt>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError, { enableHighAccuracy: true });
+} else {
+    console.log('Geolocation is not supported.');
+}
+
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(`Latitude: ${latitude}°, Longitude: ${longitude}°`);
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            break;
+    }
+}
